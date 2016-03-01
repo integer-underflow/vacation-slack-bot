b61f276e7838ddaed8ce3720828054c27eca1b51
Ice5050 <ice50505050@hotmail.com>
2016-03-01 17:05:33 +0700

    Fix load database.yml can not parse env and adding database.yml for heroku

--- vacationbot.rb
+++ vacationbot.rb
@@ -3,6 +3,7 @@ require "bundler/setup"
 require "active_record"
 require 'google/apis/calendar_v3'
 require 'signet/oauth_2/client'
+require 'erb'
 
 require 'slack-ruby-bot'
 require_relative 'vacation-bot/commands/add'
@@ -12,7 +13,7 @@ Dir[File.join(".", "models/*.rb")].each{|f| require f}
 Dir[File.join(".", "models/**/*.rb")].each{|f| require f}
 
 vacation_slack_bot_env = ENV['VACATION_SLACK_BOT_ENV'] || 'development'
-connection_details = YAML::load(File.open("config/database.#{vacation_slack_bot_env}.yml"))
+connection_details = YAML.load(ERB.new(File.read("config/database.#{vacation_slack_bot_env}.yml")).result)
 ActiveRecord::Base.establish_connection(connection_details)
 
 VacationBot::Bot.run
